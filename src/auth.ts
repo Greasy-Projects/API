21cad894275670a400ff81ba3d2db99acb579868
veryCrunchy <verycrunchydev@gmail.com>
2024-02-16 22:03:31 +0100

    feat: improve jwt validation

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,9 @@
 import { OAuth2Client } from "oslo/oauth2";
 import { TimeSpan, createDate } from "oslo";
+import { validateJWT } from "oslo/jwt";
+import { schema, db } from "./db";
+import { secret } from "./main";
+import { and, eq } from "drizzle-orm";
 
 const discordAuthorizeEndpoint = "https://discord.com/oauth2/authorize";
 const discordTokenEndpoint = "https://discord.com/api/oauth2/token";
@@ -140,4 +144,44 @@ export interface AuthProvider {
   ): Promise<URL>;
   validateAuthorizationCode(code: string): Promise<Tokens>;
   refreshAccessToken?(refreshToken: string): Promise<Tokens>;
+}
+
+type payload = { u: string; a: string };
+export async function verifyAuth(request: Request | string): Promise<payload> {
+  let token: string;
+  if (typeof request !== "string") {
+    const header = request.headers.get("authorization");
+    if (!header) {
+      throw new Error("Missing Authorization header");
+    }
+
+    const parts = header.split(" ");
+    if (parts.length !== 2 || parts[0] !== "Bearer")
+      throw new Error("Invalid Authorization header");
+    token = parts[1];
+  } else token = request;
+
+  let payload: payload;
+  // Make sure the token is valid and has not been tampered with
+  try {
+    // This function will throw an error incase of; invalid signature, expired token or inactive token (`nbf`)
+    // https://oslo.js.org/reference/jwt/validateJWT
+    const jwt = await validateJWT("HS256", secret, token);
+    payload = jwt.payload as payload;
+  } catch (e) {
+    throw new Error("Invalid token");
+  }
+
+  if (!payload.a || !payload.u) throw new Error("Invalid token payload");
+
+  const [session] = await db
+    .selectDistinct()
+    .from(schema.session)
+    .where(
+      and(eq(schema.session.userId, payload.u), eq(schema.session.token, token))
+    );
+
+  if (!session) throw new Error("Invalid session");
+
+  return payload;
 }
--- src/gql/resolvers.ts
+++ src/gql/resolvers.ts
@@ -1,12 +1,11 @@
 import { InferResolvers } from "garph";
 import { YogaInitialContext } from "graphql-yoga";
 
-import { mutationType, queryType } from "./schema";
+import {  queryType } from "./schema";
 import { db, schema } from "../db";
-import { user } from "../db/schema";
-import { secret } from "../main";
-import { validateJWT } from "oslo/jwt";
-import { eq } from "drizzle-orm";
+
+import {  eq } from "drizzle-orm";
+import { verifyAuth } from "../auth";
 
 type Resolvers = InferResolvers<
   { Query: typeof queryType },
@@ -17,7 +16,7 @@ export const resolvers: Resolvers = {
   Query: {
     hello: () => "Hello World!",
     me: async (_, __, { request }) => {
-      let data = await protectedRoute(request);
+      let data = await verifyAuth(request);
 
       const [user] = await db
         .selectDistinct()
@@ -41,28 +40,3 @@ export const resolvers: Resolvers = {
     },
   },
 };
-
-type payload = { u: string; a: string };
-async function protectedRoute(request: Request): Promise<payload> {
-  const header = request.headers.get("authorization");
-  if (!header) {
-    throw new Error("Missing Authorization header");
-  }
-
-  const parts = header.split(" ");
-  if (parts.length !== 2 || parts[0] !== "Bearer")
-    throw new Error("Invalid Authorization header");
-  const token = parts[1];
-
-  //make sure the token is valid and has not been tampered with
-  try {
-    let jwt = await validateJWT("HS256", secret, token);
-    let payload = jwt.payload as payload;
-
-    if (!payload.a || !payload.u) throw new Error("Invalid token.");
-
-    return payload;
-  } catch (e) {
-    throw new Error("Invalid token.");
-  }
-}
