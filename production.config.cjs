177d529b4e31b9b5f60e30701c77d4bb4aa5868b
veryCrunchy <verycrunchydev@gmail.com>
2024-02-22 00:29:25 +0100

    fix: start script

--- production.config.cjs
+++ production.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
       name: "GreasyGang Prod API",
       port: "5010",
       script: "pnpm",
-      args: "run start",
+      args: "start",
       autorestart: true,
       env: {
         NODE_ENV: "production", // Set the NODE_ENV environment variable to production
