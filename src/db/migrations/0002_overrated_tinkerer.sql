c5d325356d81e59f1930ded20350f3897d9224f5
veryCrunchy <verycrunchydev@gmail.com>
2024-04-10 02:32:23 +0200

    chore: generate migration files

--- src/db/migrations/0002_overrated_tinkerer.sql
+++ src/db/migrations/0002_overrated_tinkerer.sql
@@ -1,12 +1,11 @@
 CREATE TABLE `minecraft_users` (
-	`minecraft_uuid` varchar(36) NOT NULL,
+	`id` varchar(36) NOT NULL,
 	`user_id` varchar(100) NOT NULL,
+	`whitelisted` boolean DEFAULT true,
 	`created_at` datetime NOT NULL,
 	`updated_at` datetime NOT NULL,
-	CONSTRAINT `minecraft_users_minecraft_uuid` PRIMARY KEY(`minecraft_uuid`),
-	CONSTRAINT `minecraft_users_minecraft_uuid_unique` UNIQUE(`minecraft_uuid`)
+	CONSTRAINT `minecraft_users_id` PRIMARY KEY(`id`),
+	CONSTRAINT `minecraft_users_id_unique` UNIQUE(`id`)
 );
---> statement-breakpoint
-ALTER TABLE `sessions` DROP FOREIGN KEY `sessions_user_id_users_id_fk`;
 --> statement-breakpoint
 ALTER TABLE `minecraft_users` ADD CONSTRAINT `minecraft_users_user_id_users_id_fk` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE cascade ON UPDATE no action;
