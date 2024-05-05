718091e582bcea7ecdec920e1aa5092dabe58079
veryCrunchy <verycrunchydev@gmail.com>
2024-05-05 17:58:21 +0200

    feat: watchtime counting

--- src/watchtime.ts
+++ src/watchtime.ts
@@ -4,6 +4,8 @@ import { db, watchtime } from "./db";
 import { sql } from "drizzle-orm";
 import { toSQLDate } from "./util";
 const streamer = "greasymac";
+
+// IN TESTING
 export default async () => {
 	const startOfMonth = new Date();
 	startOfMonth.setUTCDate(1);
@@ -26,40 +28,12 @@ export default async () => {
 	// we check for 4 and 9 minutes to account for api and db delays since we run at a 5 minute interval
 	const WHEN_FIVE_THEN = sql`WHEN ${watchtime.updatedAt} < ${FiveMinutesAgo} THEN`;
 	const WHEN_TEN_THEN = sql`WHEN ${watchtime.updatedAt} < ${TenMinutesAgo} THEN`;
-	// 2024-05-05T16:28:34.000Z  <
-	// 2024-05-05 15:37:40.596
-	await db
-		.insert(watchtime)
-		.values({
-			twitchId: "1111",
-			date: startOfMonth,
-			updatedAt: sql`${toSQLDate(new Date())}`,
-		})
-		.onDuplicateKeyUpdate({
-			set: {
-				time: sql`CASE ${WHEN_TEN_THEN} ${watchtime.time} ${WHEN_FIVE_THEN} COALESCE(${watchtime.time}, 0) + 5 ELSE ${watchtime.time} END`,
-				updatedAt: sql`CASE ${WHEN_FIVE_THEN} ${toSQLDate(new Date())} ELSE ${watchtime.updatedAt} END`,
-			},
-		});
 
 	console.log(await db.select().from(watchtime));
-	// const values = [
-	// 	{
-	// 		twitchId: "abc123",
-	// 		time: 100,
-	// 	},
-	// 	{
-	// 		twitchId: "def456",
-	// 		time: 200,
-	// 	},
-	// ];
-	// await db.insert(watchtime).values(values).onDuplicateKeyUpdate;
 
-	return;
 	try {
 		const token = await getToken(streamer);
 
-		// return console.log(await twitchAuth.refreshAccessToken(token.refresh_token));
 		const live = await axios.get("https://api.twitch.tv/helix/streams", {
 			params: {
 				user_login: streamer,
@@ -72,9 +46,6 @@ export default async () => {
 		if (!live.data.data.length) return;
 
 		if (live.data?.data[0]?.type === "live") {
-			// https://dev.twitch.tv/docs/api/reference/#get-chatters
-			// TO-DO: Missing required paramter "moderator_id", access token needs to be a moderator of broadcaster
-
 			const chatters = await axios.get(
 				"https://api.twitch.tv/helix/chat/chatters",
 				{
@@ -88,7 +59,24 @@ export default async () => {
 					},
 				}
 			);
-			console.log(chatters.data);
+			const viewers = chatters.data.data as { user_id: string }[];
+			const values = [];
+			for (const viewer of viewers) {
+				values.push({
+					twitchId: viewer.user_id,
+					date: startOfMonth,
+					updatedAt: sql`${toSQLDate(new Date())}`,
+				});
+			}
+			await db
+				.insert(watchtime)
+				.values(values)
+				.onDuplicateKeyUpdate({
+					set: {
+						time: sql`CASE ${WHEN_TEN_THEN} ${watchtime.time} ${WHEN_FIVE_THEN} COALESCE(${watchtime.time}, 0) + 5 ELSE ${watchtime.time} END`,
+						updatedAt: sql`CASE ${WHEN_FIVE_THEN} ${toSQLDate(new Date())} ELSE ${watchtime.updatedAt} END`,
+					},
+				});
 		}
 	} catch (e) {
 		console.log("something went wrong whilst updating watchtime:", e);
